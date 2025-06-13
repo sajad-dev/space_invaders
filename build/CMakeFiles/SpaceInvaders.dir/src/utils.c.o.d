c330a24536cbd40fc1126593c151a2d75933bc1a
Sajad pourajam <prj.sajad85@gmail.com>
2025-06-13 08:56:24 +0330

    Update git ignore

--- build/CMakeFiles/SpaceInvaders.dir/src/utils.c.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/SpaceInvaders.dir/src/utils.c.o: \
- /home/sajad/Documents/Programming/space_invaders/src/utils.c \
- /usr/include/stdc-predef.h \
- /home/sajad/Documents/Programming/space_invaders/src/include/utils.h
